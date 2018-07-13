ce6ec26cc194a0abfc27d9c26f8b4240b9f470a4
Felipe Acquati <unomais@yahoo.com.br>
2018-07-13 20:04:23 +0000

    Added: git-search component, git-users method in GitSearchService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
-export class AppComponent {
-  title = 'Github browser';
+
+export class AppComponent implements OnInit {
+  constructor (private GitSearchService : GitSearchService) {
+
+  }
+  
+  ngOnInit() {
+    
+    this.GitSearchService.gitUsers('tom').then( (response) => {
+      alert("Total Users Found:" + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText);
+    });
+  }
+  
+  title = 'GitHub Browser';
 }
--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,30 +1,57 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { GitSearch } from './git-search';
+import { GitUsers } from './git-users';
+import { HttpClient } from '@angular/common/http';
+//import 'rxjs/add/operator/toPromise';
+import 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GitSearchService {
-
   cachedValues: Array<{
-          [query: string]: GitSearch
-      }> = [];
-
-  constructor() { 
-    
-  }
+    [query: string]: GitSearch
+  }> = [];
   
-  gitSearch = (query: string) => {
-    let promise = new Promise((resolve, reject) => {
+  cachedValues2: Array<{
+      [query: string]: GitUsers
+  }> = [];
+  
+  constructor(private http: HttpClient) {
+      
+  }
+
+  gitSearch = (query: string): Promise<GitSearch> => {
+    let promise = new Promise<GitSearch>((resolve, reject) => {
         if (this.cachedValues[query]) {
             resolve(this.cachedValues[query])
         }
         else {
-            resolve("Placeholder");
+            this.http.get('https://api.github.com/search/repositories?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitSearch)
+            }, (error) => {
+                reject(error);
+            });
         }
-    });
-    
+    })
     return promise;
-
+  }
+  
+  gitUsers = (query: string): Promise<GitUsers> => {
+    let promise = new Promise<GitUsers>((resolve, reject) => {
+        if (this.cachedValues2[query]) {
+            resolve(this.cachedValues2[query])
+        }
+        else {
+            this.http.get('https://api.github.com/search/users?q=' + query)
+            .toPromise()
+            .then( (response) => {
+                resolve(response as GitUsers)
+            }, (error) => {
+                reject(error);
+            })
+        }
+    })
+    return promise;
   }
 }
