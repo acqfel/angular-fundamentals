83112d4a7337735fcec48f58463d01aa4c6b6644
Felipe Acquati <unomais@yahoo.com.br>
2018-08-28 00:46:35 +0000

    Added: unified-search service & interface.

---
+++ src/app/unified-search.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { UnifiedSearch } from './unified-search';
+import { Observable } from 'rxjs';
+import { GitSearchService } from './git-search.service'
+import { GitCodeSearchService } from './git-code-search.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnifiedSearchService {
+
+  constructor(private searchService : GitSearchService, 
+              private codeSearchService : GitCodeSearchService) { }
+              
+  unifiedSearch : Function = (query: string) : Observable<UnifiedSearch> => {
+
+  }
+  
+}
